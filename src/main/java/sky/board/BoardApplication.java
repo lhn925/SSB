41dc0547860b5e46852215cd95ac824967a53da0
lhn925 <lim642666@gmail.com>
2023-05-04 10:59:43 +0900

    Test:파일 추가 및 로컬 commit

--- src/main/java/sky/board/BoardApplication.java
+++ src/main/java/sky/board/BoardApplication.java
@@ -1,13 +1,16 @@
 package sky.board;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BoardApplication {
 
     public static void main (String[] args) {
         SpringApplication.run(BoardApplication.class, args);
+        log.info("이건 main 입니다");
     }
 
 }
