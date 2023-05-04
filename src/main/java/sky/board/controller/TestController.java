37952511acd3c612f695f47fe9a55fda0ae01530
lhn925 <lim642666@gmail.com>
2023-05-04 12:32:23 +0900

    Test:commit Test1

--- src/main/java/sky/board/controller/TestController.java
+++ src/main/java/sky/board/controller/TestController.java
@@ -2,10 +2,15 @@ package sky.board.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
-
+    @GetMapping("commit")
+    public String TestCommit () {
+        System.out.println("0 = " + 0);
+        return "hihi";
+    }
 
 }
