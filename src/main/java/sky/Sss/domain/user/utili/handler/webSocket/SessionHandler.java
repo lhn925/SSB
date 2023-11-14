9a934c57bf5d419adaa24c64f8a70115a51829a7
lhn925 <lim642666@gmail.com>
2023-11-14 19:03:32 +0900

    누락된 Handler commit

--- src/main/java/sky/Sss/domain/user/utili/handler/webSocket/SessionHandler.java
+++ src/main/java/sky/Sss/domain/user/utili/handler/webSocket/SessionHandler.java
@@ -1,57 +1,69 @@
 package sky.Sss.domain.user.utili.handler.webSocket;
 
-import java.lang.reflect.Type;
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.UnsupportedJwtException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import sky.Sss.domain.user.dto.HelloMessage;
+import sky.Sss.domain.user.service.login.UserLoginStatusService;
+import sky.Sss.domain.user.utili.jwt.JwtFilter;
+import sky.Sss.domain.user.utili.jwt.TokenProvider;
 
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE + 99) // 가장 높은 우선순위로 설정
 @RequiredArgsConstructor
 @Component
-public class StompSessionHandler extends StompSessionHandlerAdapter {
-    private StompSession stompSession;
-    /**
-     * 접속시
-     * @param session the client STOMP session
-     * @param connectedHeaders the STOMP CONNECTED frame headers
-     */
-    @Override
-    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-        log.info("afterConnected session.getSessionId() = {}", session.getSessionId());
-        this.stompSession = session;
-        subscribe("/login");
-    }
-    @Override
-    public Type getPayloadType(StompHeaders headers) {
-        return HelloMessage.class;
-    }
-    @Override
-    public void handleFrame(StompHeaders headers, Object payload) {
-        HelloMessage msg = (HelloMessage) payload;
-    }
-    @Override
-    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload,
-        Throwable exception) {
-        log.info("handleException exception = {}", exception.getMessage());
-    }
+public class SessionHandler implements ChannelInterceptor {
+
+    private final UserLoginStatusService userLoginStatusService;
+    private final TokenProvider tokenProvider;
+    private String redisToken;
 
     @Override
-    public void handleTransportError(StompSession session, Throwable exception) {
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        log.info(" handleTransportError xception.getMessage() = {}", exception.getMessage());
+        if (accessor.getCommand().getMessageType().equals(SimpMessageType.CONNECT)) {
+            Authentication authentication = getAuthByAuthorizationHeader(accessor);
+            Optional.ofNullable(authentication).orElseThrow(() -> {
+                throw new MessageDeliveryException("JWT");
+            });
+            UserDetails principal = (UserDetails) authentication.getPrincipal();
+            userLoginStatusService.wsIdUpdate(principal.getUsername(), redisToken, accessor.getSessionId());
+        }
+        return message;
     }
 
-    /**
-     * Subscribe.
-     *
-     * @param destination the destination
-     */
-    public synchronized void subscribe(String destination) {
-        stompSession.subscribe(destination, this);
+    public Authentication getAuthByAuthorizationHeader(StompHeaderAccessor accessor) {
+        List<String> authorization = accessor.getNativeHeader(JwtFilter.AUTHORIZATION_HEADER);
+        if (authorization != null && authorization.size() != 0) {
+            String accessToken = tokenProvider.resolveToken(authorization.get(0));
+            Boolean success = (Boolean) tokenProvider.validateAccessToken(accessToken).get("success");
+            if (success) { // redisToken
+                Jws<Claims> accessClaimsJws = tokenProvider.getAccessClaimsJws(accessToken);
+                this.redisToken = (String) accessClaimsJws.getBody().get(TokenProvider.REDIS_TOKEN_KEY);
+            }
+            return success ? tokenProvider.getAuthentication(accessToken) : null;
+        }
+        return null;
     }
+
+
 }
