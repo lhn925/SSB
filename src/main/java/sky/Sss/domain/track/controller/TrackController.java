094c12165dfe7bbde56fc1ebf2ba3ad60bd16f73
lhn925 <lim642666@gmail.com>
2023-11-20 15:58:28 +0900

    trackController 추가

---
+++ src/main/java/sky/Sss/domain/track/controller/TrackController.java
@@ -0,0 +1,22 @@
+package sky.Sss.domain.track.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/user/track")
+@RestController
+public class TrackController {
+
+    // 트랙은 하나씩
+
+    // 앨범이나 플레이리스트는 여러개
+
+
+}
