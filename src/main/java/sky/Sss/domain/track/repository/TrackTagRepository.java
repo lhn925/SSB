d899d0a6ea16d8a874355c668e2e5877ddf9375e
lhn925 <lim642666@gmail.com>
2023-11-23 13:02:07 +0900

    feat: TrackTagLinkRepository, TrackTagRepository 추가

---
+++ src/main/java/sky/Sss/domain/track/repository/TrackTagRepository.java
@@ -0,0 +1,14 @@
+package sky.Sss.domain.track.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sky.Sss.domain.track.entity.track.SsbTrackTags;
+
+public interface TrackTagRepository extends JpaRepository<SsbTrackTags, Long> {
+
+    Optional<SsbTrackTags> findByTag(String tag);
+
+    Set<SsbTrackTags> findByIdIn(Set<Long> ids);
+}
