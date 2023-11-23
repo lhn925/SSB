2fb26872b6e30174baaf7790e8daf3ebc5cf253e
lhn925 <lim642666@gmail.com>
2023-11-23 13:07:05 +0900

    fix: 역직렬화 에러 수정

--- src/main/java/sky/Sss/global/base/BaseTimeEntity.java
+++ src/main/java/sky/Sss/global/base/BaseTimeEntity.java
@@ -1,6 +1,12 @@
 package sky.Sss.global.base;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -15,10 +21,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Getter
 public class BaseTimeEntity {
     @CreatedDate
+    @JsonSerialize(using= LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class) // 직렬화
     @DateTimeFormat(pattern = "yyyy:MM:dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDateTime;
 
     @LastModifiedDate
+    @JsonSerialize(using= LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @DateTimeFormat(pattern = "yyyy:MM:dd HH:mm:ss")
     private LocalDateTime lastModifiedDateTime;
 }
