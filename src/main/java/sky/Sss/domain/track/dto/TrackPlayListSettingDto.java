b18bcc8870598781626d1546c88b45929966c228
lhn925 <lim642666@gmail.com>
2023-11-23 12:16:35 +0900

    rename:trackPlayListSettingDto

--- src/main/java/sky/Sss/domain/track/dto/TrackPlayListSettingDto.java
+++ src/main/java/sky/Sss/domain/track/dto/TrackPlayListSettingDto.java
@@ -1,27 +1,36 @@
 package sky.Sss.domain.track.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
-import lombok.AccessLevel;
+import java.util.Set;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 import sky.Sss.domain.track.model.PlayListType;
+import sky.Sss.global.utili.annotation.MultipartPictureValid;
 
 @Getter
 @Setter
 public class TrackPlayListSettingDto {
     // 플레이리스트 제목
+    @NotBlank
     private String playListTitle;
     // 플레이리스트 타입
+    @NotBlank
     private PlayListType playListType;
 
-    private MultipartFile coverImgFile;
     private String desc;
+
+    @NotBlank
     private Boolean isDownload;
+    @NotBlank
     private Boolean isPrivacy;
 
-    private List<TrackPlayListFileDto> trackPlayListFileDtoList;
+    private Set<TrackTagsDto> tagSet;
 
+    private List<TrackPlayListMetaDto> trackPlayListMetaDto;
+
+    @MultipartPictureValid
+    private MultipartFile coverImgFile;
 }
