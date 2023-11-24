576ef1de198a5f32a80c76d8e0d7ff13cb210767
lhn925 <lim642666@gmail.com>
2023-11-24 13:18:46 +0900

    fix:TrackGenre 수정

--- src/main/java/sky/Sss/domain/track/model/TrackGenre.java
+++ src/main/java/sky/Sss/domain/track/model/TrackGenre.java
@@ -1,5 +1,10 @@
 package sky.Sss.domain.track.model;
 
 public enum TrackGenre {
-    CUSTOM, NONE, AUDIO, MUSIC
+    CUSTOM("CUSTOM"), NONE("NONE"), AUDIO("AUDIO"), MUSIC("MUSIC");
+
+    private String genre;
+    TrackGenre(String value) {
+        this.genre = value;
+    }
 }
