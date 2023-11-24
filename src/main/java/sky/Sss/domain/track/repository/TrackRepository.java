b94ca78d2e91f0186f537da0281348c3be322166
lhn925 <lim642666@gmail.com>
2023-11-24 13:20:35 +0900

    feat:track 검색시 where 절에 enabled 비교 쿼리 추가

--- src/main/java/sky/Sss/domain/track/repository/TrackRepository.java
+++ src/main/java/sky/Sss/domain/track/repository/TrackRepository.java
@@ -1,6 +1,6 @@
 package sky.Sss.domain.track.repository;
 
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,14 +9,16 @@ import sky.Sss.domain.user.entity.User;
 
 public interface TrackRepository extends JpaRepository<SsbTrack, Long> {
 
-    List<SsbTrack> findByUser(User user);
-
-
     /**
      * 업로드 곡 길이 총 합
+     *
      * @param user
      * @return
      */
-    @Query("select COALESCE(sum(s.trackLength),0) from SsbTrack s where s.user = :user")
-    Integer getTotalTrackLength(@Param("user") User user);
+    @Query("select COALESCE(sum(s.trackLength),0) from SsbTrack s where s.user = :user and s.isEnabled =:isEnabled")
+    Integer getTotalTrackLength(@Param("user") User user, @Param("isEnabled") Boolean isEnabled);
+
+    @Query("select s from SsbTrack s where s.id =:id and s.user =:user and s.token =:token and s.isEnabled =:isEnabled")
+    Optional<SsbTrack> findOne(@Param("id") Long id, @Param("user") User user, @Param("token") String token,@Param("isEnabled") Boolean isEnabled);
+
 }
