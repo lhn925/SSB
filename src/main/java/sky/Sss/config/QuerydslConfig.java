3bf8b83eae119a6f2a8e7d7098a3b5f09485f186
lhn925 <lim642666@gmail.com>
2023-12-03 21:18:50 +0900

    Feat:QueryDsl Config 클래스 추가

--- src/main/java/sky/Sss/config/QuerydslConfig.java
+++ src/main/java/sky/Sss/config/QuerydslConfig.java
@@ -1,2 +1,21 @@
-package sky.Sss.config;public class QuerydslConfig {
+package sky.Sss.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class QuerydslConfig {
+
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
 }
