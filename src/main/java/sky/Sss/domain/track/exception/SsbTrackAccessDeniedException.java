2914398bf80b0d69490b1815ea887192cad73f6c
lhn925 <lim642666@gmail.com>
2024-01-13 01:59:39 +0900

    feat:SsbTrackAccessDeniedException 추가

--- src/main/java/sky/Sss/domain/track/exception/SsbTrackAccessDeniedException.java
+++ src/main/java/sky/Sss/domain/track/exception/SsbTrackAccessDeniedException.java
@@ -1,5 +1,32 @@
 package sky.Sss.domain.track.exception;
 
-public class SsbTrackPrivateException {
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+
+/**
+ * 파일 접근 제한시
+ */
+@Getter
+public class SsbTrackAccessDeniedException extends AccessDeniedException {
+
+
+    private HttpStatus httpStatus;
+    private String code;
+    public SsbTrackAccessDeniedException(String msg) {
+        super(msg);
+        this.httpStatus = HttpStatus.FORBIDDEN;
+        this.code = msg;
+    }
+
+    public SsbTrackAccessDeniedException(String msg,HttpStatus httpStatus) {
+        super(msg);
+        this.code = msg;
+        this.httpStatus = httpStatus;
+    }
+
+    public SsbTrackAccessDeniedException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
 }
