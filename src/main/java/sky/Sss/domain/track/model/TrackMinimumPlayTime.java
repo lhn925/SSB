a45b098d1ab7d8615686f58e980ee54d7160587a
lhn925 <lim642666@gmail.com>
2024-01-13 11:47:17 +0900

    feat:TrackMinimumPlayTime EnumClass description 삭제

--- src/main/java/sky/Sss/domain/track/model/TrackMinimumPlayTime.java
+++ src/main/java/sky/Sss/domain/track/model/TrackMinimumPlayTime.java
@@ -1,17 +1,12 @@
 package sky.Sss.domain.track.model;
 
-public enum TrackMiniNumPlayTime {
+public enum TrackMinimumPlayTime {
     MINI_NUM_SECOND(60);
-    
-    private final int seconds;
 
-    TrackMinimumPlayTime(String description, int seconds) {
-        this.description = description;
+    private int seconds;
+
+    TrackMinimumPlayTime(int seconds) {
         this.seconds = seconds;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public int getSeconds() {
