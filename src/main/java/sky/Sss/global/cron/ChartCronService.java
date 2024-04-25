1921ba041665ff0e79d6688358d3020b10331971
lhn925 <lim642666@gmail.com>
2024-04-25 11:52:42 +0900

    fix: ranking Cron 수정

--- src/main/java/sky/Sss/global/cron/ChartCronService.java
+++ src/main/java/sky/Sss/global/cron/ChartCronService.java
@@ -1,6 +1,7 @@
 package sky.Sss.global.cron;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -22,16 +23,15 @@ public class ChartCronService {
     private final TrackPlayMetricsService trackPlayMetricsService;
 
     private final SimpMessagingTemplate messagingTemplate;
+
     /**
      * 정각마다 현재 시간대에 조회수 총합
      */
     // fixedDelay 이전 호출이 완료된 후 부터 시작
     @Transactional
-    @Scheduled(cron = "0 0 0-23 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
+//    @Scheduled(cron = "0 0 0-23 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
     public void hourlyTotal() {
         LocalDateTime rankingDateTime = LocalDateTime.now().minusHours(Hour.HOUR_01.getValue());
-        log.info("rankingDateTime = {}", rankingDateTime);
-
         int ranDayTime = DayTime.getDayTime(rankingDateTime);
         // track 시간대 별 종합 조회수 생성
         trackPlayMetricsService.addTrackHourlyTotalPlays(ranDayTime);
@@ -45,7 +45,7 @@ public class ChartCronService {
      */
     // fixedDelay 이전 호출이 완료된 후 부터 시작
     @Transactional
-    @Scheduled(cron = "2 0 13 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
+//    @Scheduled(cron = "2 0 13 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
     public void dailyTotal() {
         LocalDateTime rankingDateTime = LocalDateTime.now().minusHours(Hour.HOUR_01.getValue());
         // 24시간 전
@@ -55,35 +55,54 @@ public class ChartCronService {
         // track 시간대 별 종합 조회수 생성
         trackPlayMetricsService.addTrackDailyTotalPlays(startDayTime, endDayTime);
     }
+
     /**
-     * 시간대별 차트
-     * 최근 24시간 50% + 현재시간대 50%
+     * 시간대별 차트 생성 및
+     * 13시 일 경우 일간차트 생성
+     * 정각마다 일간 조회수 총합 후 생성
+     * 전날 12 ~ 금일 12시까지의 데이터 총합후
+     * 13 시에 업데이트
+     *
      */
     @Transactional
-    @Scheduled(cron = "4 0 0-23 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
-    public void rankingHour() {
+    @Scheduled(cron = "0 0 0-23 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
+    public void rankingCal() {
         LocalDateTime rankingDateTime = LocalDateTime.now().minusHours(Hour.HOUR_01.getValue());
+
         int ranDayTime = DayTime.getDayTime(rankingDateTime);
         // 24시간 전
         int startDayTime = DayTime.getDayTime(rankingDateTime, Hour.HOUR_24);
         // 24시간 마지막 시간대
         int endDayTime = DayTime.getDayTime(rankingDateTime, Hour.HOUR_01);
+
         PageRequest pageRequest = PageRequest.of(0, 500);
+
+        // 시간대별 조회수 집계
+        trackPlayMetricsService.addTrackHourlyTotalPlays(ranDayTime);
+
+        // 차트 생성
         trackPlayMetricsService.addChartHourly(ranDayTime, startDayTime, endDayTime, pageRequest);
+
+        // 13 정각일 경우
+        // 일간 차트 등록
+        if (rankingDateTime.getHour() == 12) {
+            trackPlayMetricsService.addTrackDailyTotalPlays(startDayTime, endDayTime);
+            trackPlayMetricsService.addChartDaily(ranDayTime, startDayTime, pageRequest);
+        }
     }
+
     /**
      * 일간 차트
      * 매일 낮 12시 기준 최근 24시간 이용량 집계 후 13시에 업데이트
      */
     @Transactional
-    @Scheduled(cron = "5 0 13 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
+//    @Scheduled(cron = "5 0 13 * * *")  //    초 - 분 - 시 - 일 - 월 - 요일
     public void rankingDaily() {
         LocalDateTime rankingDateTime = LocalDateTime.now().minusHours(Hour.HOUR_01.getValue());
         int dayTime = DayTime.getDayTime(rankingDateTime);
         int prevDayTime = DayTime.getDayTime(rankingDateTime, Hour.HOUR_24);
         trackPlayMetricsService.addChartDaily(dayTime, prevDayTime, PageRequest.of(0, 500));
     }
-
 
 //    /**
 //     * 일간 차트
--- src/test/java/sky/Sss/global/cron/ChartCronServiceTest.java
+++ src/test/java/sky/Sss/global/cron/ChartCronServiceTest.java
@@ -1,12 +1,17 @@
 package sky.Sss.global.cron;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import sky.Sss.domain.track.dto.track.chart.TrackTotalPlaysDto;
+import sky.Sss.domain.track.model.Hour;
 import sky.Sss.domain.track.service.track.play.TrackDailyTotalPlaysService;
+import sky.Sss.domain.track.service.track.play.TrackPlayMetricsService;
+import sky.Sss.global.utili.DayTime;
 
 @SpringBootTest
 class ChartCronServiceTest {
@@ -17,6 +22,10 @@ class ChartCronServiceTest {
     @Autowired
     TrackDailyTotalPlaysService trackDailyTotalPlaysService;
 
+
+    @Autowired
+    TrackPlayMetricsService trackPlayMetricsService;
+
     @Test
     void hourlyTotal() {
         chartCronService.hourlyTotal();
@@ -24,20 +33,29 @@ class ChartCronServiceTest {
 
     @Test
     void dailyTotal() {
-        chartCronService.dailyTotal();
+        chartCronService.rankingCal();
     }
 
     @Test
     void rankingHour() {
-        chartCronService.rankingHour();
+        LocalDateTime rankingDateTime = LocalDateTime.of(2024, Month.APRIL, 24, 16, 0)
+            .minusHours(Hour.HOUR_01.getValue());
+        int ranDayTime = DayTime.getDayTime(rankingDateTime);
+        // 24시간 전
+        int startDayTime = DayTime.getDayTime(rankingDateTime, Hour.HOUR_24);
+        // 24시간 마지막 시간대
+        int endDayTime = DayTime.getDayTime(rankingDateTime, Hour.HOUR_01);
+        PageRequest pageRequest = PageRequest.of(0, 500);
+        trackPlayMetricsService.addChartHourly(ranDayTime, startDayTime, endDayTime, pageRequest);
+
     }
 
     @Test
     void rankingDaily() {
         chartCronService.rankingDaily();
     }
-    
-    
+
+
     @Test
     public void test() {
         List<TrackTotalPlaysDto> dailyTotalPlays = trackDailyTotalPlaysService.getDailyTotalPlays(2024021614,
